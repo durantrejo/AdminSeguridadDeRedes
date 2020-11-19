27248cd66b2c2a2314c2800192c35c7eab1360e0
Alejandro Osuna <pablo.osuna16@tectijuana.edu.mx>
2020-11-18 17:17:34 -0800

    se subieron los archivos

---
+++ ProyectoRedes/EnviarMsg.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title></title>
+</head>
+<body>
+  <form >
+
+    <input type="text" placeholder="name" name="name">
+    <input type="mail" placeholder="email" name="email">
+    <input type="text" placeholder="asunto" name="asunto">
+    <textarea placeholder="mensaje" name="msg"></textarea>
+  </form>
+</body>
+</html>
